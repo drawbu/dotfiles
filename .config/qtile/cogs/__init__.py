a1e3371ca42235eac57aac61b53c9e09e3a539ad
Clément <clement2104.boillot@gmail.com>
2023-05-01 17:32:30 +0200

    :bug: Fix mouse import

--- .config/qtile/cogs/__init__.py
+++ .config/qtile/cogs/__init__.py
@@ -1,6 +1,7 @@
 from .startup import startup
 from .groups import groups
 from .keys import keys, META
+from .mouse import mouse
 
 
-__all__ = ("startup", "groups", "keys", "META")
+__all__ = ("startup", "groups", "keys", "META", "mouse")
