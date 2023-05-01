198097e3101c3a3cc1c0b41314f37fe03d3a485f
Clément <clement2104.boillot@gmail.com>
2023-05-01 21:39:26 +0200

    :recycle: Avoid repetition with custom qtile bar

--- .config/qtile/cogs/screens.py
+++ .config/qtile/cogs/screens.py
@@ -1,50 +1,44 @@
-from libqtile import bar, layout, widget
-from libqtile.config import Match, Screen
+from typing import List
+
+from libqtile import bar, widget
+from libqtile.config import Screen
+
+
+class CustomBar(bar.Bar):
+    def __init__(self, is_primary: bool):
+        widgets: List[widget.base._Widget] = [
+            widget.CurrentLayout(),
+            widget.GroupBox(),
+            widget.Prompt(),
+            widget.WindowName(),
+            widget.Chord(
+                chords_colors={
+                    "launch": ("#ff0000", "#ffffff"),
+                },
+                name_transform=lambda name: name.upper(),
+            ),
+        ]
+
+        if is_primary:
+            widgets.append(widget.Systray())
+
+        widgets.extend([
+            widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+            widget.QuickExit(),
+        ])
+        super().__init__(widgets=widgets, size=24)
 
 
 screens = [
     Screen(
         wallpaper="~/assets/wallpaper.jpg",
         wallpaper_mode="fill",
-        bottom=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.Systray(),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
-            ],
-            size=24,
-        ),
+        bottom=CustomBar(True),
     ),
 
     Screen(
         wallpaper="~/assets/wallpaper.jpg",
         wallpaper_mode="fill",
-        bottom=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
-            ],
-            size=24,
-        ),
+        bottom=CustomBar(False),
     )
 ]
