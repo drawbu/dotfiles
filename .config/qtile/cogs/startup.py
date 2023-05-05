d6e0a15989830038d7bce7d6a9e11eaea0aca628
Clément <clement2104.boillot@gmail.com>
2023-05-05 18:06:56 +0200

    :bug: Fix autostart

--- .config/qtile/cogs/startup.py
+++ .config/qtile/cogs/startup.py
@@ -1,15 +1,18 @@
 import subprocess
 import os
 
-from libqtile import hook
+from libqtile import hook, qtile
 
-
-SCRIPT_PATH = "~/autostart.sh"
+SCRIPT_PATH = "~/startup.sh"
 
 
 @hook.subscribe.startup_once
 def autostart():
-    if not os.path.exists(SCRIPT_PATH):
-        return
     home = os.path.expanduser(SCRIPT_PATH)
-    subprocess.call([home])
+    if not os.path.exists(home):
+        return
+    result = subprocess.call(["sh", home])
+    if result == 1:
+        qtile.cmd_spawn("notify-send 'Error while starting'")
+        return
+    qtile.cmd_spawn("notify-send 'Started with success'")
