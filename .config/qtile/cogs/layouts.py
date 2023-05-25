dbb5a8f2f34f882f7dc44d9a3f716214be6a2ca4
Clément <clement2104.boillot@gmail.com>
2023-05-25 22:40:51 +0200

    :truck: Move layout config in a separate file

--- .config/qtile/cogs/layouts.py
+++ .config/qtile/cogs/layouts.py
@@ -1,11 +1,11 @@
 from libqtile import layout
 from libqtile.config import Match
 
+
 layouts = [
     layout.Tile(margin=3, shift_windows=True, name="Tile", add_on_top=False),
     layout.Floating(name="Float"),
 ]
-
 
 floating_layout = layout.Floating(
     float_rules=[
