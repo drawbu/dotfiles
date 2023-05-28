92276e86cbc84b2b795731edf8d74cf75d8fcc49
Clément <clement2104.boillot@gmail.com>
2023-05-28 18:42:16 +0200

    :bug: remove buggy/useless qtile functions to resize windows

--- .config/qtile/cogs/keys.py
+++ .config/qtile/cogs/keys.py
@@ -17,17 +17,11 @@ keys = [
     Key([META], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([META], "f", lazy.window.toggle_floating(), desc="Toggle floating"),
     Key([META, "control"], "Right",
-        lazy.layout.grow_right(),
-        lazy.layout.grow(),
         lazy.layout.increase_ratio(),
-        lazy.layout.delete(),
         desc="Increase active window size."
     ),
     Key([META, "control"], "Left",
-        lazy.layout.grow_left(),
-        lazy.layout.shrink(),
         lazy.layout.decrease_ratio(),
-        lazy.layout.add(),
         desc="Decrease active window size."
     ),
 ]
