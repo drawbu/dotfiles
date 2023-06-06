a0db3a3252f8edd0e7129ed136b5eb2248d46ff8
Clément <clement2104.boillot@gmail.com>
2023-06-06 11:07:23 +0200

    :wrench: Add random wallpaper on qtile

--- .config/qtile/cogs/screens.py
+++ .config/qtile/cogs/screens.py
@@ -1,16 +1,30 @@
+import os
+import os.path
+import random
+from typing import Final
+
 from libqtile.config import Screen
 
 from .bar import Bar
 
 
+def choose_wallpaper() -> str:
+    folder = os.path.expanduser("~/assets/wallpapers")
+    collection = f"{folder}/minimalistic-wallpaper-collection/images"
+    if os.path.isdir(collection):
+        return f"{collection}/{random.choice(os.listdir(collection))}"
+    return f"{folder}/wallpaper.jpg"
+
+WALLPAPER: Final[str] = choose_wallpaper()
+
 screens = [
     Screen(
-        wallpaper="~/assets/wallpaper.jpg",
+        wallpaper=WALLPAPER,
         wallpaper_mode="fill",
         bottom=Bar(True),
     ),
     Screen(
-        wallpaper="~/assets/wallpaper.jpg",
+        wallpaper=WALLPAPER,
         wallpaper_mode="fill",
         bottom=Bar(False),
     ),
