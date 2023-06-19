e4ad1716b9e8240c89dad094860ceb7b17202ecd
Clément <clement2104.boillot@gmail.com>
2023-06-19 22:49:57 +0200

    :wrench: Update nvim config

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -1,6 +1,2 @@
 lua require('drawbu')
 
-hi SpellBad ctermbg=NONE ctermfg=NONE cterm=underline
-hi SpellCap ctermbg=NONE ctermfg=NONE cterm=underline
-hi SpellLocal ctermbg=NONE ctermfg=NONE cterm=underline
-hi SpellRare ctermbg=NONE ctermfg=NONE cterm=underline
--- .config/nvim/lua/drawbu/settings.lua
+++ .config/nvim/lua/drawbu/settings.lua
@@ -7,3 +7,19 @@ vim.opt.relativenumber = true
 vim.wo.colorcolumn = '80'
 vim.opt.cursorline = true
 
+vim.opt.smartindent = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.clipboard = "unnamedplus"
+
