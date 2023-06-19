2ef7388f57224927f3af39a5b09e7e45651823e0
Clément <clement2104.boillot@gmail.com>
2023-06-19 22:47:51 +0200

    :wrench: Add noice to nvim

--- .config/nvim/lua/drawbu/plugins.lua
+++ .config/nvim/lua/drawbu/plugins.lua
@@ -1,56 +1,65 @@
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  
-  -- Visual Git
-  use {
-    'tanvirtin/vgit.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    -- Visual Git
+    use {
+        'tanvirtin/vgit.nvim',
+        requires = {
+            'nvim-lua/plenary.nvim'
+        }
     }
-  }
-  
-  -- One Dark Pro colorscheme
-  use 'navarasu/onedark.nvim'
-  
-  -- Syntax Highlighing
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function()
-      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-      ts_update()
-    end
-  }
 
-  -- Highlight other uses of the word
-  use 'RRethy/vim-illuminate'
-  
-  -- Highlight arguments' definitions and usages
-  use 'm-demare/hlargs.nvim'
+    -- One Dark Pro colorscheme
+    use 'navarasu/onedark.nvim'
 
-  -- Lualine
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
-  
-  -- Catppuccin theme
-  use { 
-    'catppuccin/nvim',
-    as = 'catppuccin',
-    config = function()
-      vim.cmd('colorscheme catppuccin')
-    end
-  }
+    -- Syntax Highlighing
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end
+    }
 
-  -- Wakatime integration
-  use 'wakatime/vim-wakatime'
+    -- Highlight other uses of the word
+    use 'RRethy/vim-illuminate'
 
-  -- Telescope
-  use {
-    'nvim-telescope/telescope.nvim', 
-    tag = '0.1.2',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    -- Highlight arguments' definitions and usages
+    use 'm-demare/hlargs.nvim'
+
+    -- Lualine
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+
+    -- Catppuccin theme
+    use { 
+        'catppuccin/nvim',
+        as = 'catppuccin',
+        config = function()
+            vim.cmd('colorscheme catppuccin')
+        end
+    }
+
+    -- Wakatime integration
+    use 'wakatime/vim-wakatime'
+
+    -- Telescope
+    use {
+        'nvim-telescope/telescope.nvim', 
+        tag = '0.1.2',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    -- Nice cmdLine
+    use {
+        'folke/noice.nvim',
+        requires = {
+            {'MunifTanjim/nui.nvim'},
+            {'rcarriga/nvim-notify'},
+        }
+    }
 end)
 
