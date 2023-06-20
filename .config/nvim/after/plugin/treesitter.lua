b2da2787ae213d697554d682cdfafd067c53576f
Clément <clement2104.boillot@gmail.com>
2023-06-20 15:58:30 +0200

    :wrench: Add only the minimum treesitter config

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "python", "lua", "vim", "nix", "javascript", "typescript", "svelte" },
+  ensure_installed = { "lua", "vim", "nix" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
