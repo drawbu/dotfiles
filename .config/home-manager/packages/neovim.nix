1c192ed9ee1a0137871dc72221f34e301fd18fd4
Clément <clement2104.boillot@gmail.com>
2023-06-23 15:49:42 +0200

    :truck: Move neovim to a brand new file

---
+++ .config/home-manager/packages/neovim.nix
@@ -0,0 +1,39 @@
+{ pkgs, ... }: {
+  programs.neovim = {
+    enable = true;
+    extraConfig = ''
+      lua require('drawbu')
+    '';
+    plugins = with pkgs.vimPlugins; [
+      # Syntax highlighting
+      nvim-treesitter.withAllGrammars
+    ];
+
+    extraPackages = with pkgs; [
+      tree-sitter
+      nodejs
+
+      # Language Servers
+      # Bash
+      nodePackages.bash-language-server
+      # Lua
+      lua-language-server
+      # Nix
+      nil
+      nixpkgs-fmt
+      statix
+      # Python
+      pyright
+      black
+      # C
+      llvmPackages_latest.clang
+      clang-tools
+      # Typescript
+      nodePackages.typescript-language-server
+      # Web (ESLint, HTML, CSS, JSON)
+      nodePackages.vscode-langservers-extracted
+      # Telescope tools
+      ripgrep
+    ];
+  };
+}
