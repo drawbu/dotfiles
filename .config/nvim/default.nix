c8a68f51d1daedbd22568c66c60f7b1dc6707d87
Clément <clement2104.boillot@gmail.com>
2023-06-26 10:23:15 +0200

    :wrench: Remove unused deps

--- .config/nvim/default.nix
+++ .config/nvim/default.nix
@@ -13,28 +13,16 @@
     extraPackages = with pkgs; [
       git
       tree-sitter
-      nodejs
       unzip
 
       # Language Servers
-      # Bash
-      nodePackages.bash-language-server
       # Lua
       lua-language-server
       # Nix
       nil
-      nixpkgs-fmt
-      statix
-      # Python
-      pyright
-      black
       # C
       llvmPackages_latest.clang
       clang-tools
-      # Typescript
-      nodePackages.typescript-language-server
-      # Web (ESLint, HTML, CSS, JSON)
-      nodePackages.vscode-langservers-extracted
       # Telescope tools
       ripgrep
     ];
