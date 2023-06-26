61067a79a5f59c1caa3d5162f5b7d293eb2780e1
Clément <clement2104.boillot@gmail.com>
2023-06-26 10:08:05 +0200

    :wrench: Remove noice

--- .config/nvim/lua/drawbu/plugins.lua
+++ .config/nvim/lua/drawbu/plugins.lua
@@ -51,15 +51,6 @@ return require('packer').startup(function(use)
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
-    -- Nice cmdLine
-    use {
-        'folke/noice.nvim',
-        requires = {
-            {'MunifTanjim/nui.nvim'},
-            {'rcarriga/nvim-notify'},
-        }
-    }
-
     use 'mbbill/undotree'
 
     use {
