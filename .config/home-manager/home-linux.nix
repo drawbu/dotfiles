947073846c03c780c4f7b9cd4866387b34b2e680
Gabriel Guillou <gabriel@pop-os.localdomain>
2023-07-14 15:53:35 +0200

    work in progress

--- .config/home-manager/home-linux.nix
+++ .config/home-manager/home-linux.nix
@@ -1,15 +1,14 @@
 { config, pkgs, ... }: {
 
   imports = [
-    ./packages
     ./packages/games.nix
   ];
 
   programs.home-manager.enable = true;
 
   home = {
-    username = "clement";
-    homeDirectory = "/home/clement";
+    username = "gabriel";
+    homeDirectory = "/home/gabriel";
 
     stateVersion = "23.05";
 
@@ -23,7 +22,43 @@
 
       # unfree
       jetbrains-toolbox
+      # cli & tui
+      direnv
+      bat
+      exa
+      wakatime
+      btop
+      neofetch
+      tmux
+      speedtest-cli
+      wget
+      git
+
+      # softwares
+      discord
+      obsidian
+      spotify
+      vscode
+      jetbrains.clion
+      jetbrains.pycharm-professional
+      jetbrains.webstorm
+
+      # fonts
+      jetbrains-mono
+      nerdfonts
     ];
+
+    imports = [
+      ./../nvim
+    ];
+    
+    programs.zsh = {
+      enable = true;
+      dotDir = ".config/zsh";
+      initExtra = ''
+        source ~/.zshrc
+      '';
+    };
   };
   nixpkgs.config.allowUnfreePredicate = (pkg: true);
 }
