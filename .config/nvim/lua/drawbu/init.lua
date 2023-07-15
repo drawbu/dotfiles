03c99c9dcbd08df8cc59ed2a68fc68748e7f7d1b
Clément <clement2104.boillot@gmail.com>
2023-07-15 22:17:36 +0200

    :construction: Add basic support for cpool nvim

--- .config/nvim/lua/drawbu/init.lua
+++ .config/nvim/lua/drawbu/init.lua
@@ -1,4 +1,13 @@
 require('drawbu.settings')
 require('drawbu.remap')
-require("lazy").setup("drawbu.lazy")
+
+-- Epitech don't want you to use plugins for the pool
+local function file_exists(name)
+   local f = io.open(name, 'r')
+   return f ~= nil and io.close(f)
+end
+
+if not file_exists('.nvim_clean') then
+    require('lazy').setup('drawbu.lazy')
+end
 
