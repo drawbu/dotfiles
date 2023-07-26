c0538c49618fe92a1c706989a7f6c39a93b48b55
Clément <clement2104.boillot@gmail.com>
2023-07-26 10:33:12 +0200

    Clear the coplilot config

--- .config/nvim/lua/drawbu/plugins/copilot.lua
+++ .config/nvim/lua/drawbu/plugins/copilot.lua
@@ -6,17 +6,8 @@ require('copilot').setup({
         enabled = false,
     },
     filetypes = {
-        yaml = false,
-        markdown = false,
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ['.'] = false,
+        yaml = true,
+        markdown = true,
     },
-    copilot_node_command = 'node', -- Node.js version must be > 16.x
-    server_opts_overrides = {},
 })
 
