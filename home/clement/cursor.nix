7e098c14028c522de5b49407432ef71f0446c9de
Clément <clement2104.boillot@gmail.com>
2023-07-30 15:53:56 +0200

    Use catppuccin mocha dark cursor

--- home/clement/cursor.nix
+++ home/clement/cursor.nix
@@ -3,16 +3,16 @@
   home.pointerCursor = {
     gtk.enable = true;
     x11.enable = true;
-    name = "Catppuccin-Macchiato-Dark-Cursors";
-    package = pkgs.catppuccin-cursors.macchiatoDark;
+    name = "Catppuccin-Mocha-Light-Cursors";
+    package = pkgs.catppuccin-cursors.mochaLight;
     size = 24;
   };
 
   gtk = {
     enable = true;
     cursorTheme = {
-      name = "Catppuccin-Macchiato-Dark";
-      package = pkgs.catppuccin-cursors.macchiatoDark;
+      name = "Catppuccin-Mocha-Light";
+      package = pkgs.catppuccin-cursors.mochaLight;
     };
   };
 } 
