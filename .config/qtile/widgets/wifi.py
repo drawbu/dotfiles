34cd95a48209c029e122c623377fc2aa57c0f685
Clément <clement2104.boillot@gmail.com>
2023-07-30 20:16:20 +0200

    Qtile will send a notification when the network is disconnected

--- .config/qtile/widgets/wifi.py
+++ .config/qtile/widgets/wifi.py
@@ -1,6 +1,7 @@
 import subprocess
 
 from libqtile import qtile
+from libqtile.notify import notifier
 from libqtile.widget import base
 
 
@@ -8,6 +9,7 @@ class Wifi(base.InLoopPollText):
     def __init__(self, **config):
         super().__init__("", update_interval=10, qtile=qtile, **config)
         self.name = "Wifi widget"
+        self.__connected = False
 
     def poll(self) -> str:
         try:
@@ -33,7 +35,12 @@ class Wifi(base.InLoopPollText):
                 continue
             connection_type = line[type_index:device_index].strip()
             if connection_type == "ethernet":
+                self.__connected = True
                 return "󰈀 "
             if connection_type == "wifi":
+                self.__connected = True
                 return " "
+        if self.__connected:
+            self.__connected = False
+            qtile.cmd_spawn("notify-send 'Disconnected from network'")
         return "󰤯 "
