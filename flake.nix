9c1d502c5c3f2be01a06c63b1167b86022db2c3c
Clément <clement2104.boillot@gmail.com>
2023-08-02 21:17:34 +0200

    Add a working Contabo NixOS config

--- flake.nix
+++ flake.nix
@@ -40,6 +40,12 @@
             home-manager.nixosModules.home-manager { home-manager = hm; }
           ];
         };
+        "croissant" = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./croissant.nix
+          ];
+        };
       };
     };
 }
