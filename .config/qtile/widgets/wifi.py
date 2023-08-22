c2304180768220cdc07d90aebeb93cf50b07abda
Clément <clement2104.boillot@gmail.com>
2023-08-22 11:32:54 +0200

    Update wifi qtile widget

--- .config/qtile/widgets/wifi.py
+++ .config/qtile/widgets/wifi.py
@@ -1,8 +1,49 @@
 import subprocess
 
 from libqtile import qtile
-from libqtile.notify import notifier
 from libqtile.widget import base
+
+class NetworkError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+
+class NetworkState:
+    def __init__(self):
+        self.wifi: bool = False
+        self.ethernet: bool = False
+        self.refresh()
+
+    def refresh(self) -> None:
+        self.wifi = False
+        self.ethernet = False
+        try:
+            proc = subprocess.Popen(
+                ['nmcli', 'connection', 'show', '--active'],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+            )
+        except FileNotFoundError:
+            return
+        stdout, stderr = proc.communicate()
+        if stderr is not None:
+            return
+        lines = stdout.decode("utf-8").split("\n")
+        if len(lines) < 2:
+            return
+        type_index = lines[0].find("TYPE")
+        device_index = lines[0].find("DEVICE")
+        if type_index == -1 and device_index == -1 and type_index > device_index:
+            return
+        for line in lines[1:]:
+            if len(line) < device_index:
+                continue
+            connection_type = line[type_index:device_index].strip()
+            if connection_type == "ethernet":
+                self.ethernet = True
+                continue
+            if connection_type == "wifi":
+                self.wifi = True
+                continue
 
 
 class Wifi(base.InLoopPollText):
@@ -10,37 +51,29 @@ class Wifi(base.InLoopPollText):
         super().__init__("", update_interval=10, qtile=qtile, **config)
         self.name = "Wifi widget"
         self.__connected = False
+        self.__state = NetworkState()
+
+    @property
+    def connected(self) -> bool:
+        return self.__connected
+
+    @connected.setter
+    def connected(self, value: bool) -> None:
+        if self.__connected and not value:
+            qtile.cmd_spawn("notify-send 'Disconnected from network'")
+        self.__connected = value
 
     def poll(self) -> str:
         try:
-            proc = subprocess.Popen(
-                ["nmcli", "connection", "show", "--active"],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT,
-            )
-        except FileNotFoundError:
+            self.__state.refresh()
+        except NetworkError:
+            self.connected = False
             return "󰖪 "
-        stdout, stderr = proc.communicate()
-        if stderr is not None:
-            return "󰖪 "
-        lines = stdout.decode("utf-8").split("\n")
-        if len(lines) < 2:
-            return "󰖪 "
-        type_index = lines[0].find("TYPE")
-        device_index = lines[0].find("DEVICE")
-        if type_index == -1 and device_index == -1 and type_index > device_index:
-            return "󰖪 "
-        for line in lines[1:]:
-            if len(line) < device_index:
-                continue
-            connection_type = line[type_index:device_index].strip()
-            if connection_type == "ethernet":
-                self.__connected = True
-                return "󰈀 "
-            if connection_type == "wifi":
-                self.__connected = True
-                return " "
-        if self.__connected:
-            self.__connected = False
-            qtile.cmd_spawn("notify-send 'Disconnected from network'")
+        if self.__state.ethernet:
+            self.connected = True
+            return "󰈀 "
+        if self.__state.wifi:
+            self.connected = True
+            return " "
+        self.connected = False
         return "󰤯 "
