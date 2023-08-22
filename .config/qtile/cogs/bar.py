fd83bc8e5f130015a3be9f8bb37c7404f23feafe
Clément <clement2104.boillot@gmail.com>
2023-08-22 10:36:04 +0200

    Update qtile config
    Remove catppuccin flavour from bar
    
    Add comment to delimit the bar
    
    Customize widgets
    
    Move clock widget
    
    Add WidgetType
    
    Move systray in a box
    
    Add quickexit widget
    
    Add a wayyy better bar widget and add it as optional

--- .config/qtile/cogs/bar.py
+++ .config/qtile/cogs/bar.py
@@ -1,8 +1,8 @@
 from typing import List
+import subprocess
 
 from libqtile import bar, widget
 
-from plugins.catppuccin import Flavour
 from widgets import Wakatime, Separator, Wifi
 from .widget_defaults import (
     TEXT_COLOR,
@@ -12,14 +12,46 @@ from .widget_defaults import (
     FADED_COLOR,
 )
 
-flavour = Flavour.mocha()
+WidgetType = widget.base._Widget
+
+
+def has_battery() -> bool:
+        try:
+            proc = subprocess.run(["upower", "-e"])
+        except FileNotFoundError:
+            return False
+        return proc.returncode == 0
 
 
 class Bar(bar.Bar):
     def __init__(self, is_primary: bool):
-        separators_width: int = 15
+        self.__sep_width: int = 15
 
-        widgets: List[widget.base._Widget] = [
+        box_items : List[WidgetType] = [
+            self.sep(),
+            widget.QuickExit(default_text='[X]', countdown_format='[{}]'),
+            self.sep(),
+        ]
+
+        if is_primary:
+            box_items = [
+                self.sep(),
+                widget.Systray(),
+            ] + box_items
+
+        battery_widgets: List[WidgetType] = []
+
+        if has_battery():
+            battery_widgets = [
+                widget.Battery(show_short_text=False, foreground="#FFFFFF", format="{char}", full_char=" ", charge_char=" ", discharge_char = ' ', update_interval=5),
+                widget.Battery(show_short_text=False, format="{percent:2.0%}", notify_below = 30, notification_timeout = 0, update_interval=5),
+                widget.Battery(show_short_text=False, format="{char}", full_char="", charge_char="󰁞", discharge_char = '󰁆', update_interval=5),
+                self.sep(),
+            ]
+
+
+        widgets: List[WidgetType] = [
+            # Left part
             widget.CurrentLayout(),
             widget.GroupBox(
                 borderwidth=0,
@@ -37,12 +69,20 @@ class Bar(bar.Bar):
                 this_screen_border=INACTIVE_COLOR,
             ),
             widget.Prompt(bell_style="visual"),
+
+            # Middle part
+            self.sep(),
             widget.TaskList(
                 highlight_method="block",
                 border=INACTIVE_COLOR,
                 rounded=False,
                 max_title_width=200,
+                txt_floating="󱂬 ",
+                txt_minimized="󰖰 ",
             ),
+            self.sep(),
+
+            # Right part
             widget.Chord(
                 chords_colors={
                     "launch": ("#ff0000", "#ffffff"),
@@ -50,30 +90,27 @@ class Bar(bar.Bar):
                 name_transform=lambda name: name.upper(),
             ),
             Wakatime(),
-            Separator(padding=separators_width),
-            widget.Battery(
-                charge_char="󰂄",
-                full_char="󱊣",
-                discharge_char="󱧥",
-                empty_char="",
-                format="{char} {percent:2.0%} {hour:d}:{min:02d}"
-            ),
-            Separator(padding=separators_width),
+            self.sep(),
+        ] + battery_widgets + [
             Wifi(),
-            Separator(padding=separators_width),
+            self.sep(),
             widget.PulseVolume(),
+            self.sep(),
+            widget.Clock(format="%A, %b %-d"),
+            self.sep(),
+            widget.Clock(markup=True, format="<span foreground='#FFFFFF'>󰥔 </span>%I:%M %p"),
+            self.sep(),
+            widget.WidgetBox(
+                widgets=box_items,
+                foreground=PRIMARY_COLOR,
+                text_closed=" ",
+                text_open="  ",
+                close_button_location="right",
+            ),
+            self.sep(),
         ]
 
-        if is_primary:
-            widgets.extend([
-                widget.Systray(),
-                Separator(padding=separators_width)
-            ])
-
-        widgets.extend(
-            [
-                widget.Clock(format="%a %I:%M %p"),
-                Separator(padding=separators_width),
-            ]
-        )
         super().__init__(widgets=widgets, size=24, background=BACKGROUND_COLOR + "F3")
+
+    def sep(self):
+        return Separator(padding=self.__sep_width)
