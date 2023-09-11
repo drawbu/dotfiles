4d0d62c06f4ee5626ad96ee882d90624c70671e7
Clément <clement2104.boillot@gmail.com>
2023-09-11 10:28:55 +0200

    Embracing Nix for zsh config

--- .zshrc
+++ .zshrc
@@ -1,35 +1,31 @@
-export ZSH="$HOME/.ohmyzsh"
-
 ZSH_THEME="drawbu"
 
 COMPLETION_WAITING_DOTS="true"
 HIST_STAMPS="mm/dd/yyyy"
-ZSH_CUSTOM=$HOME/.config/omz
-
-plugins=(
-  git
-  brew
-  direnv
-  wakatime
-  zsh-autosuggestions
-  zsh-syntax-highlighting
-  zsh-autocomplete
-  nix-zsh-completions
-)
 
 # powerline10k
-source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-source $ZSH_CUSTOM/themes/powerlevel10k/powerlevel10k.zsh-theme
 source $HOME/assets/.p10k.zsh
 
-source $ZSH/oh-my-zsh.sh
-source $HOME/assets/.profile
+# Load oh my zsh if not loaded
+if [[ -z "$ZSH" ]]; then
+  ZSH_CUSTOM=$HOME/.config/omz
+  plugins=(
+    git
+    brew
+    direnv
+    wakatime
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+    zsh-autocomplete
+    nix-zsh-completions
+  )
+  ZSH_CUSTOM=$HOME/.config/omz
+  source $ZSH_CUSTOM/themes/powerlevel10k/powerlevel10k.zsh-theme
 
-if [ -x "$(command -v brew)" ]; then
-  if type brew &>/dev/null; then
-    FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
-
-    autoload -Uz compinit
-    compinit
-  fi
+  source $ZSH/oh-my-zsh.sh
+  export ZSH="$HOME/.oh-my-zsh"
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
+
+# Load custom shell config for any shell
+source $HOME/assets/.profile
--- home/clement/zsh.nix
+++ home/clement/zsh.nix
@@ -1,9 +1,71 @@
-{ ... }: {
+{ pkgs, lib, ... }: {
   programs.zsh = {
     enable = true;
     dotDir = ".nix-zsh";
     initExtra = ''
       source ~/.zshrc
     '';
+    plugins = [
+      {
+        name = "wakatime";
+        src = pkgs.fetchFromGitHub {
+          owner = "sobolevn";
+          repo = "wakatime-zsh-plugin";
+          rev = "69c6028b0c8f72e2afcfa5135b1af29afb49764a";
+          hash = "sha256-pA1VOkzbHQjmcI2skzB/OP5pXn8CFUz5Ok/GLC6KKXQ=";
+        };
+      }
+      {
+        name = "zsh-autosuggestions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-autosuggestions";
+          rev = "v0.7.0";
+          hash = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w=";
+        };
+      }
+      {
+        name = "zsh-syntax-highlighting";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-syntax-highlighting";
+          rev = "0.7.1";
+          hash = "sha256-gOG0NLlaJfotJfs+SUhGgLTNOnGLjoqnUp54V9aFJg8=";
+        };
+      }
+      {
+        name = "zsh-autocomplete";
+        src = pkgs.fetchFromGitHub {
+          owner = "marlonrichert";
+          repo = "zsh-autocomplete";
+          rev = "eee8bbeb717e44dc6337a799ae60eda02d371b73";
+          hash = "sha256-2qkB8I3GXeg+mH8l12N6dnKtdnaxTeLf5lUHWxA2rNg=";
+        };
+      }
+      {
+        name = "nix-zsh-completions";
+        src = pkgs.fetchFromGitHub {
+          owner = "nix-community";
+          repo = "nix-zsh-completions";
+          rev = "0.5.0";
+          hash = "sha256-DKvCpjAeCiUwD5l6PUW7WlEvM0cNZEOk41IiVXoh9D8=";
+        };
+      }
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "direnv"
+      ];
+    };
   };
+
+  home.file.".zshrc".source = ../../.zshrc;
 }
