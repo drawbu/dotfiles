892e357a9a0ccbd8c200acedf5dd15c3fb09a165
Clément <clement2104.boillot@gmail.com>
2023-09-15 22:53:45 +0200

    Create home for clement user

--- nixos/default.nix
+++ nixos/default.nix
@@ -9,6 +9,7 @@
     openssh.authorizedKeys.keys = [
       ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMEIMRXQMnaP08FgRGXEpgX9oDADom5h+xjAnEgLNCXF clement2104.boillot@gmail.com''
     ];
+    createHome = true;
   };
 
   nix = {
