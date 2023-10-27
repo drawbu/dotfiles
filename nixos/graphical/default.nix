ff03c897bf5cb5beacc19216a0feb11d2e877ef6
Clément <clement2104.boillot@gmail.com>
2023-10-27 19:17:30 +0200

    Add KDEConnect

--- nixos/graphical/default.nix
+++ nixos/graphical/default.nix
@@ -44,10 +44,14 @@
     printing.enable = true;
   };
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  programs = {
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    };
+
+    kdeconnect.enable = true;
   };
 
   environment = {
