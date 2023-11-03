12cda7e4e5a6a9db3a44ec9cf8f899c046c84269
Clément <clement2104.boillot@gmail.com>
2023-11-03 11:38:11 +0100

    Improve nvim config

--- home/clement/nvim/lua/drawbu/settings.lua
+++ home/clement/nvim/lua/drawbu/settings.lua
@@ -1,11 +1,14 @@
 local vim = vim
 
+-- ↓ Lines numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- ↓ Line at 80 chars
 vim.wo.colorcolumn = '80'
 vim.opt.cursorline = true
 
+-- ↓ Indent
 vim.opt.smartindent = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -16,14 +19,20 @@ vim.g.python_indent = {
     closed_paren_align_last_line = true,
 }
 
+-- ↓ All my homies hate swapfile
 vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
 vim.opt.undofile = true
 vim.opt.swapfile = false
 
+-- ↓ Search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.scrolloff = 8
+-- ↓ Curor never touching borders
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99 -- Minimum number of screen line below and above the cursor
+vim.opt.foldenable = true
+vim.opt.scrolloff = 99
 
 vim.cmd('filetype plugin on')
 
