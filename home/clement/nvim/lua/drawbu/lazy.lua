72373f71dc4202c3aec5d1ae4a04add6acf2b32e
Clément <clement2104.boillot@gmail.com>
2023-11-12 16:18:42 +0100

    Improve nvim-tree

--- home/clement/nvim/lua/drawbu/lazy.lua
+++ home/clement/nvim/lua/drawbu/lazy.lua
@@ -152,7 +152,22 @@ return {
         'nvim-tree/nvim-tree.lua',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         config = function()
-            require('nvim-tree').setup()
+            require('nvim-tree').setup({
+                filters = {
+                    dotfiles = true,
+                    git_ignored = false,
+                    custom = { '^\\.git' },
+                },
+                renderer = {
+                    icons = {
+                        glyphs = {
+                            git = {
+                                ignored = "-",
+                            }
+                        }
+                    }
+                },
+            })
         end,
     },
 
