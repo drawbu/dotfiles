9fb6eeee988c36e8db6ecbe211d41eca867c20d7
Clément <clement2104.boillot@gmail.com>
2024-01-14 17:41:25 +0100

    Fix xdg

--- home/clement/mimeapps.nix
+++ home/clement/mimeapps.nix
@@ -3,26 +3,29 @@ let
   mailApp = "thunderbird.desktop";
 in
 {
-  xdg.mimeApps = {
+  xdg = {
     enable = true;
-    defaultApplications = {
-      "image/*" = [ "org.gnome.eog.desktop" "feh.desktop" ];
-      "text/*" = "nvim.desktop";
-      "video/*" = "vlc.desktop";
-    };
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "image/*" = [ "org.gnome.eog.desktop" "feh.desktop" ];
+        "text/*" = "nvim.desktop";
+        "video/*" = "vlc.desktop";
+      };
 
-    associations.added = {
-      "text/html" = "firefox.desktop";
-      "text/xml" = "firefox.desktop";
-      "application/zip" = "org.gnome.FileRoller.desktop";
-      "x-scheme-handler/jetbrains" = "jetbrains-toolbox.desktop";
+      associations.added = {
+        "text/html" = "firefox.desktop";
+        "text/xml" = "firefox.desktop";
+        "application/zip" = "org.gnome.FileRoller.desktop";
+        "x-scheme-handler/jetbrains" = "jetbrains-toolbox.desktop";
 
-      # ↓ Mails
-      "application/x-extension-ics" = mailApp;
-      "x-scheme-handler/mailto" = mailApp;
-      "x-scheme-handler/mid" = mailApp;
-      "x-scheme-handler/webcal" = mailApp;
-      "x-scheme-handler/webcals" = mailApp;
+# ↓ Mails
+        "application/x-extension-ics" = mailApp;
+        "x-scheme-handler/mailto" = mailApp;
+        "x-scheme-handler/mid" = mailApp;
+        "x-scheme-handler/webcal" = mailApp;
+        "x-scheme-handler/webcals" = mailApp;
+      };
     };
   };
 }
