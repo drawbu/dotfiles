a9cb7f41873cf1c7edeaa2934f2d3f78a0767a53
Clément <clement2104.boillot@gmail.com>
2024-01-31 14:52:18 +0100

    Improve waybar config

--- home/clement/hypr/waybar.nix
+++ home/clement/hypr/waybar.nix
@@ -1,13 +1,8 @@
-{ pkgs, ... }:
+{ ... }:
 let
   colors = import ./colors.nix { };
 in
 {
-  home.packages = with pkgs; [
-    material-design-icons
-    material-icons
-    material-symbols
-  ];
   programs.waybar =
     with colors;{
       enable = true;
@@ -18,15 +13,30 @@ in
       style = ''
         window#waybar {
           background-color: #${darker};
-          color: #${foreground};
           border-bottom: none;
         }
-        #workspaces {
-          font-family: "Material Design Icons Desktop";
-          font-size: 20px;
+        * {
+          color: #${foreground};
+          font-size: 16px;
+          min-height: 0;
+          font-family: "Iosevka Nerd Font", "Material Design Icons Desktop";
+        }
+
+        /* left widgets */
+
+        #custom-launcher {
           background-color: #${background};
-          margin : 4px 0;
-          border-radius : 5px;
+          color: #${raw.blue};
+          margin: 4px 0 4px 4px;
+          padding: 5px 12px;
+          font-size: 18px;
+          border-radius: 5px;
+        }
+        #workspaces {
+          background-color: #${background};
+          margin: 4px 0 4px 4px;
+          font-size: 20px;
+          border-radius: 5px;
         }
         #workspaces button {
           font-size: 18px;
@@ -42,76 +52,95 @@ in
           color: #${raw.yellow};
           font-size: 12px;
         }
-        #custom-launcher {
-          background-color: #${background};
-          color: #${raw.blue};
-          margin : 4px 4.5px;
-          padding : 5px 12px;
-          font-size: 18px;
-          border-radius : 5px;
-        }
-        #custom-power {
-          background-color: #${background};
-          color : #${raw.red};
-          margin : 4px 4.5px 4px 4.5px;
-          padding : 5px 11px 5px 13px;
-          border-radius : 5px;
-        }
+
+        /* center widgets */
 
         #clock {
           background-color: #${background};
-          color: #${foreground};
-          margin : 4px 9px;
-          padding : 5px 12px;
-          border-radius : 5px;
+          margin: 4px 4px 4px 4px;
+          padding: 5px 12px;
+          border-radius: 5px;
         }
-        
+        #privacy {
+          background-color: #${background};
+          margin: 4px 0 4px 0;
+          padding: 5px 12px;
+          border-radius: 5px;
+        }
+
+        /* right widgets */
+
+        #pulseaudio {
+          background-color: #${background};
+          margin: 4px 0 4px 0;
+          padding: 5px 12px;
+          border-radius: 5px 0 0 5px;
+        }
+        #bluetooth {
+          background-color: #${background};
+          margin: 4px 0 4px 0;
+          padding: 5px 12px;
+        }
+        #bluetooth.connected {
+          color: #${raw.blue};
+        }
         #network {
           background-color: #${background};
-          color : #${raw.blue};
-          margin : 4px 0 4px 4.5px;
-          padding : 5px 12px;
-          border-radius : 5px 0 0 5px;
+          margin: 4px 0 4px 0;
+          padding: 5px 12px;
         }
         #battery {
           background-color: #${background};
-          color : #${raw.mauve};
-          margin : 4px 0px;
-          padding : 5px 12px;
-          border-radius : 5px 0 0 5px;
+          margin: 4px 4px 4px 0;
+          padding: 5px 12px;
+          border-radius: 0 5px 5px 0;
         }
-        #custom-swallow {
+        #battery.charging {
+          color: #${raw.green};
+        }
+        @keyframes blink {
+          to {
+            background-color: #ffffff;
+            color: black;
+          }
+        }
+        #battery.warning:not(.charging) {
+          color: #${raw.yellow};
+        }
+        #battery.critical:not(.charging) {
+          background: #${raw.red};
+          color: white;
+          animation-name: blink;
+          animation-duration: 0.5s;
+          animation-timing-function: steps(12);
+          animation-iteration-count: infinite;
+          animation-direction: alternate;
+        }
+        #tray {
           background-color: #${background};
-          margin : 4px 4.5px;
-          padding : 5px 12px;
-          border-radius : 5px;
+          margin: 4px 4px 4px 0;
+          padding: 5px 11px 5px 13px;
+          border-radius: 5px;
         }
-        * {
-          font-size: 16px;
-          min-height: 0;
-          font-family: "Iosevka Nerd Font", "Material Design Icons Desktop";
+        #custom-power {
+          background-color: #${background};
+          color: #${raw.red};
+          margin: 4px 4px 4px 0;
+          padding: 5px 11px 5px 13px;
+          border-radius: 5px;
         }
       '';
       settings = [{
         height = 35;
         layer = "top";
         position = "top";
-        tray = { spacing = 10; };
 
+        # ↓ Left widgets
         modules-left = [
           "custom/launcher"
           "hyprland/workspaces"
         ];
-        modules-center = [
-          "clock"
-        ];
-        modules-right = [
-          "network"
-          "battery"
-          "custom/power"
-          "tray"
-        ];
-
+        "custom/launcher" = { format = " "; };
         "hyprland/workspaces" = {
           on-click = "activate";
           all-outputs = true;
@@ -119,8 +148,8 @@ in
           disable-scroll = true;
           active-only = false;
           format-icons = {
-            default = "󰊠 ";
-            active = "󰮯 ";
+            default = "";
+            active = "";
           };
           persistent_workspaces = {
             "1" = [ ];
@@ -130,20 +159,43 @@ in
             "5" = [ ];
           };
         };
-        battery = {
-          format = "{icon}";
-          format-charging = "󰂄";
-          format-icons = [ "󰂎" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
-          format-plugged = "󰚦 ";
-          states = {
-            critical = 15;
-            warning = 30;
-          };
-          tootip-format = "{capacity} - {time}";
-        };
+
+        # ↓ Center widgets
+        modules-center = [
+          "clock"
+          "privacy"
+        ];
         clock = {
           format = "{:%d %A %H:%M}";
           tooltip-format = "{:%Y-%m-%d | %H:%M}";
+        };
+
+        # ↓ Right widgets
+        modules-right = [
+          "pulseaudio"
+          "bluetooth"
+          "network"
+          "battery"
+          "tray"
+          "custom/power"
+        ];
+        pulseaudio = {
+          format = "{icon}";
+          format-bluetooth = "{icon}";
+          format-muted = "";
+          format-icons = { "default" = ["" ""]; };
+          on-click = "pavucontrol";
+          tooltip-format = "{volume}%";
+        };
+        bluetooth = {
+          format = "";
+          format-connected = " {device_alias}";
+          format-connected-battery = " {device_alias} {device_battery_percentage}%";
+          tooltip-format = "{controller_alias}\t{controller_address}\n\n{num_connections} connected";
+          tooltip-format-connected = "{controller_alias}\t{controller_address}\n\n{num_connections} connected\n\n{device_enumerate}";
+          tooltip-format-enumerate-connected = "{device_alias}\t{device_address}";
+          tooltip-format-enumerate-connected-battery = "{device_alias}\t{device_address}\t{device_battery_percentage}%";
+          on-click = "rofi-bluetooth";
         };
         network = {
           interval = 1;
@@ -151,14 +203,26 @@ in
           format-disconnected = "󰤮 ";
           format-ethernet = "󰈀 ";
           format-wifi = "󰤨 ";
-          tootip-format = "{essid} - {bandwidthDownBytes} ↓ {bandwidthUpOctets} ↑";
+          tootip-format = "{essid}\n\n{bandwidthDownBytes} ↓\n{bandwidthUpOctets} ↑";
+          tooltip-format-wifi = "{essid}\t({signalStrength}%)\n\n{bandwidthDownBytes} ↓\n{bandwidthUpOctets} ↑";
+          tooltip-format-disconnected = "No connection";
         };
-        "custom/launcher" = {
-          format = " ";
+        battery = {
+          format = "{icon}";
+          format-charging = "󰂄";
+          format-icons = [ "󰂎" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
+          format-plugged = "󰚦 ";
+          states = {
+            critical = 10;
+            warning = 20;
+          };
+          tooltip-format = "{capacity}% - {time}";
         };
+        tray = { spacing = 10; };
         "custom/power" = {
           on-click = "powermenu";
           format = " ";
+          tooltip-format = "Power menu";
         };
       }];
     };
