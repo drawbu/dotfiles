7af40a007c5de1cb5f28a6cebd778b26719f6910
Clément <clement2104.boillot@gmail.com>
2024-02-22 11:01:46 +0100

    Fix tmux nvim plugin

--- home/clement/tmux.nix
+++ home/clement/tmux.nix
@@ -1,13 +1,13 @@
 { pkgs, ... }:
 let
-  tmuxNvim = pkgs.tmuxPlugins.mkTmuxPlugin {
-    pluginName = "tmux.nvim";
-    version = "unstable-2023-10-28";
+  vimTmuxNavigator = pkgs.tmuxPlugins.mkTmuxPlugin rec {
+    pluginName = "vim-tmux-navigator";
+    version = "unstable-2023-12-23";
     src = pkgs.fetchFromGitHub {
-      owner = "aserowy";
-      repo = "tmux.nvim";
-      rev = "ea67d59721eb7e12144ce2963452e869bfd60526";
-      hash = "sha256-/2flPlSrXDcNYS5HJjf8RbrgmysHmNVYYVv8z3TLFwg=";
+      owner = "christoomey";
+      repo = pluginName;
+      rev = "38b1d0402c4600543281dc85b3f51884205674b6";
+      hash = "sha256-4WpY+t4g9mmUrRQgTmUnzpjU8WxtrJOWzIL/vY4wR3I=";
     };
   };
 in
@@ -20,8 +20,31 @@ in
     secureSocket = true;
 
     plugins = with pkgs.tmuxPlugins; [
-      sensible
-      tmuxNvim
+      { plugin = sensible; }
+      { 
+        plugin = vimTmuxNavigator;
+        extraConfig = ''
+          # Smart pane switching with awareness of Vim splits.
+          # See: https://github.com/christoomey/vim-tmux-navigator
+          is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
+              | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|fzf)(diff)?$'"
+          bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
+          bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
+          bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
+          bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
+          tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
+          if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
+              "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
+          if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
+              "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
+
+          bind-key -T copy-mode-vi 'C-h' select-pane -L
+          bind-key -T copy-mode-vi 'C-j' select-pane -D
+          bind-key -T copy-mode-vi 'C-k' select-pane -U
+          bind-key -T copy-mode-vi 'C-l' select-pane -R
+          bind-key -T copy-mode-vi 'C-\' select-pane -l
+        '';
+      }
       {
         plugin = catppuccin;
         extraConfig = ''
