8bebda3d1762aba4f9dcc7094c9dcc77ff7d7d00
Clément <clement2104.boillot@gmail.com>
2024-03-14 14:48:21 +0100

    Set firefox as default browser

--- home/clement/mimeapps.nix
+++ home/clement/mimeapps.nix
@@ -1,6 +1,20 @@
 { ... }:
 let
   mailApp = "thunderbird.desktop";
+  browser = rec {
+    name = "firefox.desktop";
+    settings = {
+      "x-scheme-handler/http" = name;
+      "x-scheme-handler/https" = name;
+      "x-scheme-handler/chrome" = name;
+      "application/x-extension-htm" = name;
+      "application/x-extension-html" = name;
+      "application/x-extension-shtml" = name;
+      "application/xhtml+xml" = name;
+      "application/x-extension-xhtml" = name;
+      "application/x-extension-xht" = name;
+    };
+  };
 in
 {
   xdg = {
@@ -11,7 +25,8 @@ in
         "image/*" = [ "org.gnome.eog.desktop" "feh.desktop" ];
         "text/*" = "nvim.desktop";
         "video/*" = "vlc.desktop";
-      };
+        "x-scheme-handler/msteams" = "teams-for-linux.desktop";
+      } // browser.settings;
 
       associations.added = {
         "text/html" = "firefox.desktop";
@@ -25,7 +40,8 @@ in
         "x-scheme-handler/mid" = mailApp;
         "x-scheme-handler/webcal" = mailApp;
         "x-scheme-handler/webcals" = mailApp;
-      };
+        "x-scheme-handler/msteams" = "teams-for-linux.desktop";
+      } // browser.settings;
     };
   };
 }
