907e52dc316f7949b900eed722fdfc943db608a7
Clément <clement2104.boillot@gmail.com>
2024-07-12 12:09:23 +0200

    feat: change some flatpack to nixpkgs

--- home/clement/flatpak.nix
+++ home/clement/flatpak.nix
@@ -11,19 +11,7 @@
   ];
 
   services.flatpak.packages = [
-    "dev.vencord.Vesktop"
-    "com.discordapp.Discord"
-    "org.prismlauncher.PrismLauncher"
-    "com.github.IsmaelMartinez.teams_for_linux"
-    "com.spotify.Client"
-    "com.heroicgameslauncher.hgl"
-    "md.obsidian.Obsidian"
-    "io.github.ungoogled_software.ungoogled_chromium"
-    "org.libreoffice.LibreOffice"
-    "org.videolan.VLC"
-    "com.obsproject.Studio"
     "com.github.tchx84.Flatseal"
-    "org.kde.filelight"
     "org.gnome.SimpleScan"
   ];
 }
