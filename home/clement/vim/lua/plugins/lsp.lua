ffcf1f33546602bc6099bfb61090eb7d3070b7ad
Clément <clement2104.boillot@gmail.com>
2024-09-06 20:18:29 +0200

    feat: oopsie on inlay hints keybind

--- home/clement/vim/lua/plugins/lsp.lua
+++ home/clement/vim/lua/plugins/lsp.lua
@@ -75,23 +75,12 @@ return {
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
         vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, opts)
         vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<C-l>', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, opts)
+        vim.keymap.set('n', '<leader>k', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, opts)
         vim.keymap.set('n', 'gf', vim.lsp.buf.format, opts)
         lsp_zero.default_keymaps({ buffer = bufnr })
       end)
 
       lsp_zero.setup()
-    end,
-  },
-
-  {
-    "chrisgrieser/nvim-lsp-endhints",
-    event = "LspAttach",
-    opts = {},
-    config = function()
-      require("lsp-endhints").setup {
-        autoEnableHints = false,
-      }
     end,
   },
 
@@ -125,4 +114,6 @@ return {
   'sakhnik/nvim-gdb',
 
   'tjdevries/templ.nvim',
+
+  'nvim-treesitter/nvim-treesitter-context',
 }
