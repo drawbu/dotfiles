09cf23c801619a7973546547e8fed78ce17cc493
Clément <clement2104.boillot@gmail.com>
2024-09-21 11:58:47 +0200

    feat: change icon pack with Adwaita

--- home/clement/gtk.nix
+++ home/clement/gtk.nix
@@ -30,8 +30,17 @@
     enable = true;
 
     iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
+      name = "MoreWaita";
+      package = pkgs.morewaita-icon-theme.overrideAttrs (old: {
+        version = "unstable-2024-09-19";
+        src = pkgs.fetchFromGitHub {
+          owner = "MoreWaita-Dev";
+          repo = "MoreWaita";
+          rev = "cc79d82f74034fb0cad6fe50d7fc5dee755546ba";
+          hash = "sha256-NkvrjeaIhx4aw0PC56fCxj0R+ZNKVEAhFPhzLEWGhh0=";
+        };
+        buildInputs = [pkgs.gnome.adwaita-icon-theme];
+      });
     };
 
     theme.name = "Catppuccin-Mocha-Compact-Peach-Dark";
