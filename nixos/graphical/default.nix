21241bcdd9479e72e19825c120e67745e926c61a
Clément <git@drawbu.dev>
2025-06-15 11:38:40 +0200

    feat: use french time servers

--- nixos/graphical/default.nix
+++ nixos/graphical/default.nix
@@ -91,4 +91,14 @@
     enable = true;
     polkitPolicyOwners = [ "clement" ];
   };
+
+  networking = {
+    timeServers = [
+      "0.fr.pool.ntp.org"
+      "1.fr.pool.ntp.org"
+      "2.fr.pool.ntp.org"
+      "3.fr.pool.ntp.org"
+    ];
+    stevenblack.enable = true;
+  };
 }
