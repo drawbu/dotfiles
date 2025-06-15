4994b2b5ea0066a07b586673f50799b50bbdd0ee
Clément <git@drawbu.dev>
2025-06-15 11:55:33 +0200

    chore: long overdue waybar update
    inspiration:
    https://github.com/AlexNabokikh/nix-config/blob/master/modules/home-manager/services/waybar/default.nix

--- home/clement/hypr/waybar.nix
+++ home/clement/hypr/waybar.nix
@@ -1,216 +1,192 @@
+{ ... }:
 {
-  config,
-  pkgs,
-  lib,
-  ...
-}:
-let
-  colors = import ./colors.nix { theme = "dark"; };
-  theme = pkgs.fetchFromGitHub {
-    owner = "catppuccin";
-    repo = "waybar";
-    rev = "f74ab1eecf2dcaf22569b396eed53b2b2fbe8aff";
-    hash = "sha256-WLJMA2X20E5PCPg0ZPtSop0bfmu+pLImP9t8A8V4QK8=";
-  };
-
-  activation = import ./symlink.nix {
-    inherit pkgs lib;
-    path = "${config.home.homeDirectory}/.config/waybar";
-    file = "theme.css";
-    default = "dark.css";
-  };
-in
-{
-  xdg.configFile = {
-    "waybar/dark.css".source = "${theme}/themes/mocha.css";
-    "waybar/light.css".source = "${theme}/themes/latte.css";
-  };
-  home.activation.createWaybarGTKTheme = "sh ${activation.script}";
-
-  programs.waybar = with colors; {
+  programs.waybar = {
     enable = true;
-    systemd.enable = false;
-    style = ''
-      @import "./theme.css";
-
-      window#waybar {
-        background-color: ${darker.gtk};
-        border-bottom: none;
-      }
-      * {
-        color: ${foreground.gtk};
-        font-size: 16px;
-        min-height: 0;
-        font-family: "Inter", "JetBrainsMono Nerd Font";
-      }
-
-      /* left widgets */
-
-      box > widget > label,
-      box > widget > box {
-        background-color: ${background.gtk};
-        margin-top: 4px;
-        margin-bottom: 4px;
-        padding: 5px 12px;
-        border-radius: 5px;
-      }
-
-      #custom-launcher {
-        color: ${raw.blue.gtk};
-        margin-left: 4px;
-        font-size: 18px;
-      }
-      #workspaces {
-        margin-left: 4px;
-        font-size: 20px;
-      }
-      #workspaces button {
-        color: ${raw.blue.gtk};
-        font-size: 18px;
-        background-color: transparent;
-        transition: all 0.1s ease;
-      }
-      #workspaces button.focused {
-        color: ${raw.green.gtk};
-        font-size: 18px;
-      }
-      #workspaces button.persistent {
-        color: ${raw.yellow.gtk};
-        font-size: 12px;
-      }
-
-      /* center widgets */
-
-      #clock,
-      #privacy,
-      #mpris,
-      #power-profiles-daemon {
-        margin-left: 4px;
-      }
-
-      #mpris.playing.spotify,
-      #mpris.playing.spotifyd {
-        color: ${raw.green.gtk};
-      }
-
-      /* right widgets */
-
-      #pulseaudio {
-        border-top-right-radius: 0;
-        border-bottom-right-radius: 0;
-      }
-      #bluetooth {
-        border-radius: 0;
-      }
-      #bluetooth.connected {
-        color: ${raw.blue.gtk};
-      }
-      #network {
-        border-radius: 0;
-      }
-      #battery {
-        margin-right: 4px;
-        border-top-left-radius: 0;
-        border-bottom-left-radius: 0;
-      }
-      #battery.charging {
-        color: ${raw.green.gtk};
-      }
-      @keyframes blink {
-        to {
-          background-color: #ffffff;
-          color: black;
-        }
-      }
-      #battery.warning:not(.charging) {
-        color: ${raw.yellow.gtk};
-      }
-      #battery.critical:not(.charging) {
-        background: ${raw.red.gtk};
-        color: white;
-        animation-name: blink;
-        animation-duration: 0.5s;
-        animation-timing-function: steps(12);
-        animation-iteration-count: infinite;
-        animation-direction: alternate;
-      }
-      #tray {
-        margin-right: 4px;
-      }
-    '';
-    settings = [
-      {
-        height = 35;
+    settings = {
+      mainBar = {
         layer = "top";
         position = "top";
+        exclusive = true;
+        passthrough = false;
+        fixed-center = true;
+        ipc = true;
+        margin-top = 3;
+        margin-left = 4;
+        margin-right = 4;
 
-        # ↓ Left widgets
         modules-left = [
-          "custom/launcher"
           "hyprland/workspaces"
+          "power-profiles-daemon"
+          "cpu"
+          "temperature"
+          "memory"
+          "backlight"
         ];
-        "custom/launcher" = {
-          format = " ";
-          on-click = "nwg-displays";
-        };
-        "hyprland/workspaces" = {
-          on-click = "activate";
-          all-outputs = true;
-          format = "{icon}";
-          disable-scroll = true;
-          active-only = false;
-          format-icons = {
-            default = "";
-            active = "";
-          };
-          persistent-workspaces = {
-            "1" = [ ];
-            "2" = [ ];
-            "3" = [ ];
-            "4" = [ ];
-            "5" = [ ];
-          };
-        };
 
-        # ↓ Center widgets
         modules-center = [
           "custom/dark-switch"
           "clock"
-          "privacy"
-          "mpris"
-          "power-profiles-daemon"
+          "custom/notification"
         ];
-        "custom/dark-switch" = {
-          format = "󱎖 ";
-          on-click = "dark";
-          tooltip = "Dark mode";
+
+        modules-right = [
+          "privacy"
+          "idle_inhibitor"
+          "tray"
+          "bluetooth"
+          "pulseaudio"
+          "battery"
+        ];
+
+        backlight = {
+          interval = 2;
+          align = 0;
+          rotate = 0;
+          format = "{icon} {percent}%";
+          format-icons = [
+            "󰃞"
+            "󰃟"
+            "󰃝"
+            "󰃠"
+          ];
+          icon-size = 10;
+          on-scroll-up = "brightnessctl set +5%";
+          on-scroll-down = "brightnessctl set 5%-";
+          smooth-scrolling-threshold = 1;
         };
+
+        battery = {
+          interval = 60;
+          align = 0;
+          rotate = 0;
+          full-at = 100;
+          design-capacity = false;
+          states = {
+            good = 95;
+            warning = 30;
+            critical = 20;
+          };
+          format = "<big>{icon}</big>  {capacity}%";
+          format-charging = " {capacity}%";
+          format-plugged = " {capacity}%";
+          format-full = "{icon} Full";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+          format-time = "{H}h {M}min";
+          tooltip = true;
+          tooltip-format = "{timeTo}";
+        };
+
+        bluetooth = {
+          format = "";
+          format-connected = " {num_connections}";
+          tooltip-format = " {device_alias}";
+          tooltip-format-connected = "{device_enumerate}";
+          tooltip-format-enumerate-connected = "Name: {device_alias}\nBattery: {device_battery_percentage}%";
+          on-click = "blueman-manager";
+        };
+
         clock = {
-          format = "{:%d %A %H:%M}";
-          tooltip-format = "{:%Y-%m-%d | %H:%M}";
-        };
-        mpris = {
-          format = "{player_icon} {dynamic}";
-          format-paused = "{status_icon} <i>{dynamic}</i>";
-          dynamic-order = [
-            "artist"
-            "title"
-          ];
-          dynamic-importance-order = [
-            "title"
-            "artist"
-          ];
-          dynamic-len = 30;
-          player-icons = rec {
-            default = " ";
-            spotify = " ";
-            spotifyd = spotify;
-            firefox = " ";
-          };
-          status-icons = {
-            paused = " ";
+          format = "{:%b %d %H:%M}";
+          format-alt = " {:%H:%M   %Y, %d %B, %A}";
+          tooltip-format = "<tt><small>{calendar}</small></tt>";
+          calendar = {
+            mode = "year";
+            mode-mon-col = 3;
+            weeks-pos = "right";
+            on-scroll = 1;
+            format = {
+              months = "<span color='#f5a97f'><b>{}</b></span>";
+              days = "<span color='#a5adce'><b>{}</b></span>";
+              weeks = "<span color='#8087a2'><b>W{}</b></span>";
+              weekdays = "<span color='#b7bdf8'><b>{}</b></span>";
+              today = "<span color='#ed8796'><b><u>{}</u></b></span>";
+            };
           };
         };
+
+        cpu = {
+          format = "󰍛 {usage}%";
+          interval = 5;
+        };
+
+        "hyprland/language" = {
+          format = "{short}";
+        };
+
+        "hyprland/workspaces" = {
+          all-outputs = true;
+          format = "{name}";
+          on-click = "activate";
+          show-special = false;
+          sort-by-number = true;
+        };
+
+        memory = {
+          interval = 10;
+          format = "󰾆 {used:0.1f}G";
+          tooltip = true;
+          tooltip-format = "{used:0.1f}GB/{total:0.1f}G";
+          on-click = "foot --title btop sh -c 'btop'";
+        };
+
+        privacy = {
+          icon-size = 14;
+          modules = [
+            {
+              type = "screenshare";
+              tooltip = true;
+            }
+          ];
+        };
+
+        pulseaudio = {
+          format = "{icon} {volume}%";
+          format-muted = "";
+          format-icons = {
+            default = [
+              ""
+              ""
+              " "
+            ];
+          };
+          on-click = "pavucontrol";
+          on-scroll-up = "pamixer -i 5";
+          on-scroll-down = "pamixer -d 5";
+          scroll-step = 5;
+          on-click-right = "pamixer -t";
+          smooth-scrolling-threshold = 1;
+          ignored-sinks = [ "Easy Effects Sink" ];
+        };
+
+        temperature = {
+          interval = 10;
+          tooltip = false;
+          hwmon-path = "/sys/class/hwmon/hwmon1/temp1_input";
+          critical-threshold = 82;
+          format-critical = "{icon} {temperatureC}°C";
+          format = "󰈸 {temperatureC}°C";
+        };
+
+        tray = {
+          spacing = 20;
+        };
+
+        idle_inhibitor = {
+          format = "{icon}";
+          format-icons = {
+            activated = "󰅶";
+            deactivated = "󰛊";
+          };
+          tooltip-format-activated = "Caffeine active";
+          tooltip-format-deactivated = "Caffeine unactive";
+        };
+
         power-profiles-daemon = {
           format = "{icon}";
           tooltip-format = "Power profile: {profile}\nDriver: {driver}";
@@ -223,75 +199,145 @@ in
           };
         };
 
-        # ↓ Right widgets
-        modules-right = [
-          "tray"
-          "pulseaudio"
-          "bluetooth"
-          "network"
-          "battery"
-        ];
-        tray = {
-          spacing = 10;
-        };
-        pulseaudio = {
+        "custom/notification" = {
+          tooltip = false;
           format = "{icon}";
-          format-bluetooth = "{icon}";
-          format-muted = "";
           format-icons = {
-            "default" = [
-              ""
-              ""
-            ];
+            notification = "<span foreground='red'><sup></sup></span>";
+            none = "";
+            dnd-notification = "<span foreground='red'><sup></sup></span>";
+            dnd-none = "";
+            inhibited-notification = "<span foreground='red'><sup></sup></span>";
+            inhibited-none = "";
+            dnd-inhibited-notification = "<span foreground='red'><sup></sup></span>";
+            dnd-inhibited-none = "";
           };
-          on-click = "${pkgs.pavucontrol}/bin/pavucontrol";
-          tooltip-format = "{volume}%";
+          return-type = "json";
+          exec-if = "which swaync-client";
+          exec = "swaync-client -swb";
+          on-click = "swaync-client -t -sw";
+          on-click-right = "swaync-client -d -sw";
+          escape = true;
         };
-        bluetooth = {
-          format = "";
-          format-connected = " {device_alias}";
-          format-connected-battery = " {device_alias} {device_battery_percentage}%";
-          tooltip-format = "{controller_alias}\t{controller_address}\n\n{num_connections} connected";
-          tooltip-format-connected = "{controller_alias}\t{controller_address}\n\n{num_connections} connected\n\n{device_enumerate}";
-          tooltip-format-enumerate-connected = "{device_alias}\t{device_address}";
-          tooltip-format-enumerate-connected-battery = "{device_alias}\t{device_address}\t{device_battery_percentage}%";
-          on-click = lib.getExe pkgs.rofi-bluetooth;
+
+        "custom/dark-switch" = {
+          format = "󱎖 ";
+          on-click = "dark";
+          tooltip = "Dark mode";
         };
-        network = {
-          interval = 1;
-          on-click = "ghostty -e nmtui";
-          format-disconnected = "󰤮 ";
-          format-ethernet = "󰈀 ";
-          format-wifi = "󰤨 ";
-          tootip-format = "{essid}\n\n{bandwidthDownBytes} ↓\n{bandwidthUpOctets} ↑";
-          tooltip-format-wifi = "{essid}\t({signalStrength}%)\n\n{bandwidthDownBytes} ↓\n{bandwidthUpOctets} ↑";
-          tooltip-format-disconnected = "No connection";
-        };
-        battery = {
-          interval = 5;
-          format = "{icon} {power:4.2f} W";
-          format-charging = "󰂄";
-          format-icons = [
-            "󰂎"
-            "󰁺"
-            "󰁻"
-            "󰁼"
-            "󰁽"
-            "󰁾"
-            "󰁿"
-            "󰂀"
-            "󰂁"
-            "󰂂"
-            "󰁹"
-          ];
-          format-plugged = "󰚦 ";
-          states = {
-            critical = 10;
-            warning = 20;
-          };
-          tooltip-format = "{capacity}% - {time}";
-        };
+      };
+    };
+    style = ''
+      * {
+        font-family: "JetBrainsMono Nerd Font";
+        font-weight: bold;
+        min-height: 0;
+        font-size: 100%;
+        font-feature-settings: '"zero", "ss01", "ss02", "ss03", "ss04", "ss05", "cv31"';
+        padding: 0px;
+        margin-top: 1px;
+        margin-bottom: 1px;
       }
-    ];
+
+      window#waybar {
+        background: rgba(0, 0, 0, 0);
+      }
+
+      window#waybar.hidden {
+        opacity: 0.5;
+      }
+
+      tooltip {
+        background: #24273A;
+        border-radius: 8px;
+      }
+
+      tooltip label {
+        color: #cad3f5;
+        margin-right: 5px;
+        margin-left: 5px;
+      }
+
+      .modules-right,
+      .modules-center,
+      .modules-left {
+        background-color: rgba(0, 0, 0, 0.6);
+        border: 0px solid #b4befe;
+        border-radius: 8px;
+      }
+
+      #workspaces button {
+        padding: 2px;
+        color: #6e738d;
+        margin-right: 5px;
+      }
+
+      #workspaces button.active {
+        color: #dfdfdf;
+        border-radius: 3px 3px 3px 3px;
+      }
+
+      #workspaces button.focused {
+        color: #d8dee9;
+      }
+
+      #workspaces button.urgent {
+        color: #ed8796;
+        border-radius: 8px;
+      }
+
+      #workspaces button:hover {
+        color: #dfdfdf;
+        border-radius: 3px;
+      }
+
+      #backlight,
+      #battery,
+      #bluetooth,
+      #clock,
+      #cpu,
+      #custom-notification,
+      #language,
+      #memory,
+      #privacy,
+      #pulseaudio,
+      #temperature,
+      #tray,
+      #workspaces,
+      #idle_inhibitor,
+      #power-profiles-daemon,
+      #custom-dark-switch {
+        color: #dfdfdf;
+        padding: 0px 10px;
+        border-radius: 8px;
+      }
+
+      #temperature.critical {
+        background-color: #ff0000;
+      }
+
+      @keyframes blink {
+        to {
+          color: #000000;
+        }
+      }
+
+      #taskbar button.active {
+        background-color: #7f849c;
+      }
+
+      #battery.critical:not(.charging) {
+        color: #f53c3c;
+        animation-name: blink;
+        animation-duration: 0.5s;
+        animation-timing-function: linear;
+        animation-iteration-count: infinite;
+        animation-direction: alternate;
+      }
+
+      #privacy {
+        color: #f5a97f;
+      }
+    '';
   };
 }
--- home/clement/shell/scripts/dark.nix
+++ home/clement/shell/scripts/dark.nix
@@ -18,13 +18,6 @@ in
         echo "$theme" > "$THEMEFILE"
         notify-send "Switching to $theme theme" -t 1000
 
-        # Update kitty theme
-        ln -fs "$XDG_CONFIG_HOME/kitty/$theme.conf" "$XDG_CONFIG_HOME/kitty/current-theme.conf"
-        while IFS= read -r -d "" sock; do
-          kitty @ --to "unix:$sock" set-colors -a -- "$XDG_CONFIG_HOME/kitty/current-theme.conf" \
-            || rm -f "$sock"
-        done <   <(find /tmp -maxdepth 1 -name "kitty-*" -type 's' -print0)
-
         # Update css theme
         ln -fs "$HOME/$theme.css" "$HOME/theme.css"
 
@@ -38,12 +31,6 @@ in
           gsettings set org.gnome.desktop.interface color-scheme "prefer-$theme"
           gsettings set org.gnome.desktop.interface gtk-theme "$gtk_theme"
         fi
-
-        # Update waybar theme
-        # Note: this is the last step of the script because waybar takes the
-        #       whole script into death with it
-        ln -fs "$XDG_CONFIG_HOME/waybar/$theme.css" "$XDG_CONFIG_HOME/waybar/theme.css"
-        pkill -SIGUSR2 waybar
       '';
     })
   ];
