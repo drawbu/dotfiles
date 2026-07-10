341cf4cc5dc8706d9eca9141037f075ceb62741b
Clément <git@drawbu.dev>
2026-07-10 19:57:47 +0200

    rebecca: allow to compile successfully

--- hosts/rebecca/caddy.nix
+++ hosts/rebecca/caddy.nix
@@ -3,8 +3,8 @@
   services.caddy = {
     enable = true;
     package = pkgs.caddy.withPlugins {
-      plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
-      hash = "sha256-saKJatiBZ4775IV2C5JLOmZ4BwHKFtRZan94aS5pO90=";
+      plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
+      hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg=";
     };
     email = "letsencrypt@drawbu.dev";
     extraConfig = ''
