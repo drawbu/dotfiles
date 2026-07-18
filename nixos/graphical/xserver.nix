9fcbc3cc7d9f57e4b48d36517a254a81a801fc17
Clément <git@drawbu.dev>
2026-07-18 11:49:25 +0200

    greet: replace greetd by regreet

--- nixos/graphical/xserver.nix
+++ nixos/graphical/xserver.nix
@@ -1,14 +1,13 @@
-{ config, pkgs, ... }:
+{ ... }:
 {
   services.xserver = {
     enable = false;
     xkb.layout = "us_qwerty-fr";
   };
 
-  services.greetd = {
-    enable = true;
-    settings.default_session.command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd ${config.programs.niri.package}/bin/niri-session";
-  };
+  services.greetd.enable = true;
+  programs.regreet.enable = true;
+
   services.displayManager.gdm.enable = false;
 
   programs.niri.enable = true;
