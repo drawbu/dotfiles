d53c1434dc0454ddf76a933a2192325afe4a3912
Clément <git@drawbu.dev>
2026-08-01 17:48:46 +0200

    nix: enable flox substituters
    builds some cuda stuff

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -23,9 +23,11 @@ in
       substituters = [
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org"
+        "https://cache.flox.dev"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
       ];
       keep-outputs = true;
       keep-derivations = true;
