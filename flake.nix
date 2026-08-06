415add5079d2ea7c472809bda7fec12c96306a62
Clément <git@drawbu.dev>
2026-08-06 14:02:28 +0200

    deps: update jj to 0.44.0

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
     ghostty.url = "github:ghostty-org/ghostty";
     ghostty.inputs.home-manager.follows = "home-manager";
 
-    jj.url = "github:jj-vcs/jj/v0.43.0";
+    jj.url = "github:jj-vcs/jj/v0.44.0";
     jj.inputs.nixpkgs.follows = "nixpkgs";
     jj.inputs.flake-utils.inputs.systems.follows = "systems";
 
