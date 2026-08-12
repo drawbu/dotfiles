eb678a0adbb611c6d4cf90c556411df3e7f05171
Clément <git@drawbu.dev>
2026-08-12 08:47:30 +0200

    caddy: update hash

--- hosts/rebecca/caddy.nix
+++ hosts/rebecca/caddy.nix
@@ -4,7 +4,7 @@
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
-      hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg=";
+      hash = "sha256-7GoH8YLCoPmPExQxoga2FHB58zQDoZVf1BBwkVi0SsQ=";
     };
     email = "letsencrypt@drawbu.dev";
     extraConfig = ''
